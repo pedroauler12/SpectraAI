22787ec6e03488d66f84703eac475df3f4ea9192
giovanna-britto <giovanna.vieira@sou.inteli.edu.br>
2026-02-09 23:15:04 -0300

    feat: adiciona funcoes para geracao de imagens e graficos

---
+++ src/analise_visual/__init__.py
@@ -0,0 +1,25 @@
+from .graficos import (
+    PlotSpec,
+    list_required_plots,
+    plot_histograms,
+    plot_boxplots,
+    plot_class_balance,
+    plot_confusion_matrix,
+    plot_spatial_confusion,
+    plot_examples,
+    plot_spatial_heatmap,
+    analysis_questions,
+)
+
+__all__ = [
+    "PlotSpec",
+    "list_required_plots",
+    "plot_histograms",
+    "plot_boxplots",
+    "plot_class_balance",
+    "plot_confusion_matrix",
+    "plot_spatial_confusion",
+    "plot_examples",
+    "plot_spatial_heatmap",
+    "analysis_questions",
+]
