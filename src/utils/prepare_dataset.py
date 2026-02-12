cf93d922e237985b9de94f8d0f6674036896accb
Pedro <Pedro.martins@sou.inteli.edu.br>
2026-02-12 14:17:01 -0300

    Remoção do aviso de performance warning

--- src/utils/prepare_dataset.py
+++ src/utils/prepare_dataset.py
@@ -22,14 +22,16 @@ def prepare_dataset(dataset_path: str, extracted_codes_path: str):
     all_ids_sorted = sorted(all_ids, key=len, reverse=True)
     id_pattern = '|'.join(re.escape(id_val) for id_val in all_ids_sorted)
     regex = rf'({id_pattern})'
-    df['image_id'] = df['path'].str.extract(regex)[0]
+    image_id = df['path'].str.extract(regex)[0]
 
     # Create label column
-    df['label'] = df['image_id'].apply(
+    label = image_id.apply(
         lambda x: 1 if x in extracted_codes['positivos'] else (
             0 if x in extracted_codes['negativos'] else -1
         )
     )
+
+    df = pd.concat([df, image_id.rename('image_id'), label.rename('label')], axis=1)
 
     # Drop non-feature columns and separate X and y
     X = df.drop(
