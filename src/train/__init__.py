92f661758c7a583a5602938fe5b5d36a8a727ccd
mateus-pereira-inteli <mateus.pereira@sou.inteli.edu.br>
2026-03-06 09:18:02 -0300

    feat: script de treino

--- src/train/__init__.py
+++ src/train/__init__.py
@@ -3,5 +3,6 @@ Módulo de treinamento e avaliação de modelos.
 """
 
 from .train_and_evaluate import train_and_evaluate
+from .train_cnn import train_cnn
 
-__all__ = ['train_and_evaluate']
+__all__ = ['train_and_evaluate', 'train_cnn']
